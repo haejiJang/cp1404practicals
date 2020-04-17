cde666b809c59935e3b276d89ae57a0388d9e5d8
haejiJang <haejijangkorean@users.noreply.github.com>
2020-04-18 00:18:06 +0900

    Exercise with list

--- practical4/list_exercise.py
+++ practical4/list_exercise.py
@@ -8,7 +8,7 @@ def basic_list_operations():
     print("The smallest number is ", min(numbers))
     print("The largest number is ", max(numbers))
     print("The average of the numbers is ", sum(numbers)/len(numbers))
-    
+
 basic_list_operations()
 
 def woefully_inadequate_security_checker():
