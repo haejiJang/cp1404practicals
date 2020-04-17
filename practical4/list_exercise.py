3988e264ec91b3e452b8914c4131219d4873f3e0
haejiJang <haejijangkorean@users.noreply.github.com>
2020-04-17 23:53:27 +0900

    Exercise with list

---
+++ practical4/list_exercise.py
@@ -0,0 +1,24 @@
+def basic_list_operations():
+    numbers = []
+    for i in range (5):
+        number = int(input("Number: "))
+        numbers.append(number)
+    print("The first number is ", numbers[0])
+    print("The last number is ", numbers[-1])
+    print("The smallest number is ", min(numbers))
+    print("The largest number is ", max(numbers))
+    print("The average of the numbers is ", sum(numbers)/len(numbers))
+    
+basic_list_operations()
+
+def woefully_inadequate_security_checker():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = str(input("What is your name?"))
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+woefully_inadequate_security_checker()
