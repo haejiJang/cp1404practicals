c47c15ba4361ff459845339b64415d4e535f0c02
haejiJang <haejijangkorean@users.noreply.github.com>
2020-05-18 16:42:02 +0900

    Update taxi simulator so that it does not let the user drive before choosing taxi

--- practical8/taxi_simulator.py
+++ practical8/taxi_simulator.py
@@ -13,6 +13,7 @@ def main():
     total_cost = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
 
     print("Let's drive!")
     print("q)uit, c)hoose taxi, d)rive")
@@ -23,7 +24,7 @@ def main():
             display_taxis(taxis)
             taxi_choice = int(input("Choose taxi: "))
             current_taxi = taxis[taxi_choice]
-        elif menu_choice == "d":
+        elif menu_choice == "d" and current_taxi != None:
             current_taxi.start_fare()
             distance_to_drive = float(input("Drive how far? "))
             current_taxi.drive(distance_to_drive)
