a7ea7ef5ac755ad9af09d1c12b8155254ba1e809
haejiJang <haejijangkorean@users.noreply.github.com>
2020-05-18 15:57:46 +0900

    Change price_per_km

--- practical8/taxi.py
+++ practical8/taxi.py
@@ -8,10 +8,11 @@ from practical8.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
+    price_per_km = 1.23
+
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -19,7 +20,7 @@ class Taxi(Car):
         return "{}, {}km on current fare, ${:.2f}/km".format(super().__str__(),
                                                              self.current_fare_distance,
                                                              self.price_per_km)
-
+    
     def get_fare(self):
         """Return the price for the taxi trip."""
         return self.price_per_km * self.current_fare_distance
