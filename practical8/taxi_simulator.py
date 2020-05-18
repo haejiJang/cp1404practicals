b7d9c7ab15e63cb31ff8da5ddec07028faccea66
haejiJang <haejijangkorean@users.noreply.github.com>
2020-05-18 16:37:25 +0900

    Create taxi_simulator. No usage of current_taxi = None

---
+++ practical8/taxi_simulator.py
@@ -0,0 +1,51 @@
+from practical8.car import Car
+from practical8.taxi import Taxi
+from practical8.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Write a taxi simulator program that uses your Taxi and SilverServiceTaxi
+       classes. Each time, until they quit:
+       The user should be presented with a list of available taxis and get to
+       choose one. Then they should say how far they want to drive.
+       At the end of each trip, show them the price and add it to their bill.
+       """
+    total_cost = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+
+    print("Let's drive!")
+    print("q)uit, c)hoose taxi, d)rive")
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            print("Taxis available: ")
+            display_taxis(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            current_taxi = taxis[taxi_choice]
+        elif menu_choice == "d":
+            current_taxi.start_fare()
+            distance_to_drive = float(input("Drive how far? "))
+            current_taxi.drive(distance_to_drive)
+            trip_cost = current_taxi.get_fare()
+            print("Your {} trip cost you ${:.2f}".format(current_taxi.name,
+                                                         trip_cost))
+            total_cost += trip_cost
+        else:
+            print("Invalid option")
+        print("Bill to date: ${:.2f}".format(total_cost))
+        print("q)uit, c)hoose taxi, d)rive")
+        menu_choice = input(">>> ").lower()
+
+    print("Total trip cost: ${:.2f}".format(total_cost))
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    """Display numbered list of taxis."""
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, taxi))
+
+
+main()
